3dd069501e7067101a4090a000110441d473e8e1
dosanity <anthonydo888@gmail.com>
2023-01-28 01:29:08 -0700

    update code

--- _site/js/template.js
+++ _site/js/template.js
@@ -286,7 +286,7 @@ let mybutton = document.getElementById("myBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
